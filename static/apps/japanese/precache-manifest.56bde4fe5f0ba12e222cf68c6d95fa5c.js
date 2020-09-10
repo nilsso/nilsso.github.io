676f998967483f565fa9ef6d6b3f8dea5545b391
Nils Olsson <nilso@enosis.net>
2020-09-10 15:17:14 -0700

    Lazy update

--- static/apps/japanese/precache-manifest.56bde4fe5f0ba12e222cf68c6d95fa5c.js
+++ static/apps/japanese/precache-manifest.56bde4fe5f0ba12e222cf68c6d95fa5c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "037cecd0ee1c97b24267ad1b41939272",
+    "revision": "bd2292a7bed68b4b82c20e5cffeb0b01",
     "url": "/apps/japanese/index.html"
   },
   {
-    "revision": "5a41a604b88f2dd98aa0",
+    "revision": "24106c5224a1805aeb68",
     "url": "/apps/japanese/static/css/main.fb74d370.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/apps/japanese/static/js/2.a8f84b44.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5a41a604b88f2dd98aa0",
-    "url": "/apps/japanese/static/js/main.0abc9df1.chunk.js"
+    "revision": "24106c5224a1805aeb68",
+    "url": "/apps/japanese/static/js/main.518290cf.chunk.js"
   },
   {
     "revision": "d7835b07349c82bb6e26",
